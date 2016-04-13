a508626b698fc01aeace385c33c5a5ead7f715ad
Chun-Chieh Huang <brpnts@gmail.com>
2016-04-13 13:19:31 +0800

    Commit 'expanded' generated css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
-var rename = require('gulp-rename');
 var sass = require('gulp-ruby-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
 
 
 gulp.task('sass', function () {
-  return sass('assets/scss/style.scss', {style: 'compressed', sourcemap: true})
+  return sass('assets/scss/style.scss', {style: 'expanded', sourcemap: true})
     .pipe(autoprefixer({remove: false}))
-    .pipe(rename({suffix: '.min'}))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('assets/css'));
 });
